a81226d68ce85321ec507fb7b122650839844fa1
Hieu Truong <Hieu_Truong_Quan@mfcgd.com>
2020-02-10 23:19:56 +0700

    Updates

---
+++ precache-manifest.d1f0f4e3ea23fdab5f75c60837b1fd91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f569633906e994d38b67fbb30f79517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a834564a8c544649a62",
+    "url": "/static/css/main.79908598.chunk.css"
+  },
+  {
+    "revision": "94dce593787ebb8326c6",
+    "url": "/static/js/2.78dadd51.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.78dadd51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a834564a8c544649a62",
+    "url": "/static/js/main.ba8f9691.chunk.js"
+  },
+  {
+    "revision": "e28cf15db56bff486bbf",
+    "url": "/static/js/runtime-main.05a55a92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/money-tools-app/precache-manifest.1c242c3f7ef3f013a41b38c83fa4eb1a.js"
+  "/precache-manifest.d1f0f4e3ea23fdab5f75c60837b1fd91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/money-tools-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
